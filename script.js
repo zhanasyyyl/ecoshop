7ced90f89e29c921ef6dbaa019ce41407b3a34d6
Zhanassyl <107205064+zhanasyyyl@users.noreply.github.com>
2024-02-13 23:18:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Это файл с скриптами JavaScript для сайта EcoShop
+// Функция для открытия внешнего сайта в новом окне
+function openExternal(url) {
+  window.open(url, "_blank");
+}
+
+function mailTo(email) {
+  window.open('mailto:'+email)
+}
+  
